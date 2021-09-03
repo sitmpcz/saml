6c5e9bcfab111055e5fedfbd59934353ea49f906
dubovsky <dubovsky@plzen.eu>
2021-09-03 13:23:21 +0200

    fix env dynamic variable

--- src/DI/SamlExtension.php
+++ src/DI/SamlExtension.php
@@ -13,12 +13,12 @@ class SamlExtension extends CompilerExtension
     public function getConfigSchema(): \Nette\Schema\Schema
     {
         return Expect::structure([
-            'public_IdP_key' => Expect::string()->required(),
-            'public_SP_key' => Expect::string()->required(),
-            'private_SP_key' => Expect::string()->required(),
-            'url_idp' => Expect::string()->required(),
-            'url_idp_sign_in' => Expect::string()->required(),
-            'url_idp_sign_out' => Expect::string()->required(),
+            'public_IdP_key' => Expect::string()->required()->dynamic(),
+            'public_SP_key' => Expect::string()->required()->dynamic(),
+            'private_SP_key' => Expect::string()->required()->dynamic(),
+            'url_idp' => Expect::string()->required()->dynamic(),
+            'url_idp_sign_in' => Expect::string()->required()->dynamic(),
+            'url_idp_sign_out' => Expect::string()->required()->dynamic(),
             'backlink' => Expect::string()->required(),
         ]);
     }
