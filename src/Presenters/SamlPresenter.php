47a126a02192fa124b3ce6330be868a32b969255
Dušan Homola <homola@plzen.eu>
2021-12-08 13:21:57 +0100

    Backlink - url fix

--- src/Presenters/SamlPresenter.php
+++ src/Presenters/SamlPresenter.php
@@ -75,7 +75,7 @@ class SamlPresenter extends Nette\Application\UI\Presenter
         //    $this->redirect($this->samlProvider->getBacklink());
         // }
         // relayState is saved in session backlinkUrl (check self URL !!!)
-        $saml->backlinkUrl = $this->getHttpRequest->getPost('RelayState');
+        $saml->backlinkUrl = $this->getHttpRequest()->getPost('RelayState');
     }
 
     public function actionLogout(): void
